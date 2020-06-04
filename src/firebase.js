a4cd8938ff6e090bf96881b4323e6f0e109f2ab8
Valeria <valeria.gutsuliak@gmail.com>
2020-06-04 11:50:08 -0500

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
-import "firebase/storage";
+import firebase from "@firebase/app";
+import "@firebase/auth";
+import "@firebase/database";
+import "@firebase/storage";
 
+var config = {
+  apiKey: "AIzaSyB2PRk0-sTV-pj0_IBvSwgvec6-Jb3TToM",
+  authDomain: "appslack-72ae3.firebaseapp.com",
+  databaseURL: "https://appslack-72ae3.firebaseio.com",
+  projectId: "appslack-72ae3",
+  storageBucket: "appslack-72ae3.appspot.com",
+  messagingSenderId: "672248957040",
+  // appId: "1:672248957040:web:732b299f8842d214b84cb5",
+  // measurementId: "G-1QXR8X0LP9"
+};
+firebase.initializeApp(config);
 
-var firebaseConfig = {
-    apiKey: "AIzaSyB2PRk0-sTV-pj0_IBvSwgvec6-Jb3TToM",
-    authDomain: "appslack-72ae3.firebaseapp.com",
-    databaseURL: "https://appslack-72ae3.firebaseio.com",
-    projectId: "appslack-72ae3",
-    storageBucket: "appslack-72ae3.appspot.com",
-    messagingSenderId: "672248957040",
-    appId: "1:672248957040:web:732b299f8842d214b84cb5",
-    measurementId: "G-1QXR8X0LP9"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-  export default firebase;
+export default firebase;
