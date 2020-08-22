97c095cd2744f3d5d034b274470bb9008045d929
Valeria <valeria.gutsuliak@gmail.com>
2020-08-21 21:07:32 -0500

    spinnerchange

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,10 +1,3 @@
 import React from "react";
 import { Loader, Dimmer } from "semantic-ui-react";
 
-const Spinner = () => (
-  <Dimmer active>
-    <Loader size="huge" content={"Preparing Chat..."} />
-  </Dimmer>
-);
-
-export default Spinner;
